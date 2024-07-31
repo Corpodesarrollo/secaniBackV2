eddc5f0cf8868dde7611293ecf7ea6cbe473530e
krlosguti <krodas17@hotmail.com>
2024-07-30 20:27:53 -0500

    MSUsuariosyRoles

---
+++ Core/CQRS/MSUsuariosyRoles/Commands/User/AssignUsersRoleCommand.cs
@@ -0,0 +1,26 @@
+﻿using Core.Interfaces.Services.MSUsuariosyRoles;
+using MediatR;
+
+namespace Core.CQRS.MSUsuariosyRoles.Commands.User
+{
+    public class AssignUsersRoleCommand : IRequest<int>
+    {
+        public string UserName { get; set; }
+        public IList<string> Roles { get; set; }
+    }
+
+    public class AssignUsersRoleCommandHandler : IRequestHandler<AssignUsersRoleCommand, int>
+    {
+        private readonly IIdentityService _identityService;
+
+        public AssignUsersRoleCommandHandler(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+        public async Task<int> Handle(AssignUsersRoleCommand request, CancellationToken cancellationToken)
+        {
+            var result = await _identityService.AssignUserToRole(request.UserName, request.Roles);
+            return result ? 1 : 0;
+        }
+    }
+}
---
+++ Microservicios/MSUsuariosyRoles/Authentication.Tests/AuthControllerTests.cs
@@ -0,0 +1,47 @@
+﻿using Authentication.Api.Controllers;
+using Authentication.Application.Commands.Auth;
+using Authentication.Application.DTOs;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace Authentication.Tests
+{
+    public class AuthControllerTests
+    {
+        private readonly Mock<IMediator> _mediatorMock;
+        private readonly AuthController _authController;
+
+        public AuthControllerTests()
+        {
+            _mediatorMock = new Mock<IMediator>();
+            _authController = new AuthController(_mediatorMock.Object);
+        }
+
+        [Fact]
+        public async Task Login_ReturnsOkResult_WithAuthResponseDTO()
+        {
+            // Arrange
+            var command = new AuthCommand
+            {
+                UserName = "test",
+                Password = "test"
+            };
+            var expectedResponse = new AuthResponseDTO
+            {
+                Name = "test",
+                Token = "test",
+                UserId = "test"
+            };
+            _mediatorMock.Setup(m => m.Send(It.IsAny<AuthCommand>(), default)).ReturnsAsync(expectedResponse);
+
+            // Act
+            var result = await _authController.Login(command);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnValue = Assert.IsType<AuthResponseDTO>(okResult.Value);
+            Assert.NotEmpty(returnValue.Token);
+        }
+    }
+}
