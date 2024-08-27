a1c73109e7c329501a589fa303a2174e990841d0
krlosguti <krodas17@hotmail.com>
2024-08-27 15:15:54 -0500

    update tablas parametricas add entidades

--- Core/Modelos/Common/TPEntidadExterna.cs
+++ Core/Modelos/Common/TPEntidadExterna.cs
@@ -1,18 +1,10 @@
-﻿using MediatR.NotificationPublishers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Core.Modelos.Common
+﻿namespace Core.Modelos.Common
 {
-    public class TPEntidadExterna: TPExternalEntityBase
+    public class TPEntidadExterna : TPExternalEntityBase
     {
         public string NITConCode { get; set; }
         public string NITSinCode { get; set; }
         public string DigitoVerificacion { get; set; }
-        public string RazonSocial { get; set; }
         public string CategoriaVIII { get; set; }
         public string CategoriaIX { get; set; }
         public string Email { get; set; }
