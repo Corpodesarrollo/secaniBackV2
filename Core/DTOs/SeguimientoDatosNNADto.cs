a5e36c6a5ecd375d70919040d7c0a912f1daf8a2
charlesrock96 <charlesrock96@yahoo.com>
2024-09-17 15:49:30 -0500

    funciones

---
+++ Core/DTOs/SeguimientoDatosNNADto.cs
@@ -0,0 +1,15 @@
+﻿namespace Core.DTOs
+{
+    public class SeguimientoDatosNNADto
+    {
+        public int IdNNA { get; set; }
+        public string? NombreCompleto { get; set; }
+        public DateTime? FechaNacimiento { get; set; }
+        public string? Edad { get; set; }
+        public string? Diagnostico { get; set; }
+        public DateTime? FechaIngresoEstrategia { get; set; }
+        public DateTime? FechaInicioSeguimiento { get; set; }
+        public string? TiempoTranscurrido { get; set; }
+        public int? SeguimientosRealizados { get; set; }
+    }
+}
