3cf8f5dc349cf352d19f7defeeff41cf37b5061a
charlesrock96 <charlesrock96@yahoo.com>
2024-09-23 13:54:32 -0500

    IStorageService

--- Core/Services/StorageService/IStorageService.cs
+++ Core/Services/StorageService/IStorageService.cs
@@ -9,6 +9,6 @@ namespace Core.Services.StorageService
         Task<byte[]?> DownloadFileAsync(string fileName);
         Task<List<string>> ListFilesAsync();
         Task<bool> UpdateFileAsync(byte[] fileBytes, string fileName);
-        Task<bool> UploadFileAsync(byte[] fileBytes, string fileName);
+        Task<bool> UploadFileAsync(byte[] fileBytes, string fileName, bool replace = false);
     }
 }
