987179c444a346a12ee6bf2cce379c1a9f7f6edc
krlosguti <krodas17@hotmail.com>
2024-09-27 20:17:50 -0500

    upddate modulos y permisos

--- Core/Modelos/TPModuloComponenteObjeto.cs
+++ Core/Modelos/TPModuloComponenteObjeto.cs
@@ -3,9 +3,9 @@
     public class TPModuloComponenteObjeto
     {
         public int Id { get; set; }
-        public string Nombre { get; set; }
-        public string Path { get; set; }
-        public string Icon { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Path { get; set; } = string.Empty;
+        public string Icon { get; set; } = string.Empty;
         public int? ModuloComponenteObjetoIdPadre { get; set; }
         public int Orden { get; set; }
         public int Activo { get; set; }
