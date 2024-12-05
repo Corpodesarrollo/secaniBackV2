d1bb69bbc7fbbaa2002ccad7c02ea1729f39ece1
charlesrock96 <charlesrock96@yahoo.com>
2024-12-04 20:51:30 -0500

    commit 20241204

---
+++ Core/Common/ByteArrayConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace Core.Common
+{
+    public class ByteArrayConverter : System.Text.Json.Serialization.JsonConverter<byte[]>
+    {
+        public override byte[]? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var byteList = JsonSerializer.Deserialize<List<byte>>(ref reader, options);
+            return byteList?.ToArray();
+        }
+
+        public override void Write(Utf8JsonWriter writer, byte[] value, JsonSerializerOptions options)
+        {
+            JsonSerializer.Serialize(writer, value.ToList(), options);
+        }
+    }
+}
