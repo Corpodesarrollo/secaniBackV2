602f58d2c0dcadeb554da382aa1cc3d175ee2463
charlesrock96 <charlesrock96@yahoo.com>
2025-04-08 15:18:38 -0500

    correccion

--- Core/DTOs/ExportarDetalleSeguimientoContactoDto.cs
+++ Core/DTOs/ExportarDetalleSeguimientoContactoDto.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Core.DTOs
+﻿namespace Core.DTOs
 {
     public class ExportarDetalleSeguimientoContactoDto
     {
         public string Nombre { get; set; }
-        public int? Parentesco {  get; set; }
-        public string CorreoElectronico {  get; set; }
-        public string Telefono {  get; set; }
+        public string? Parentesco { get; set; }
+        public string CorreoElectronico { get; set; }
+        public string Telefono { get; set; }
     }
 }
