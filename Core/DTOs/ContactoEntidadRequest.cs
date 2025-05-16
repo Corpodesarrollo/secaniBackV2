086bf03701d02f2fa04ab8cf0de373cf74df5202
charlesrock96 <charlesrock96@yahoo.com>
2025-05-16 01:06:31 -0500

    commit 20250516

--- Core/DTOs/ContactoEntidadRequest.cs
+++ Core/DTOs/ContactoEntidadRequest.cs
@@ -2,11 +2,12 @@
 {
     public class ContactoEntidadRequest
     {
-        public string EntidadId { get; set; }
-        public string Nombres { get; set; }
-        public string Cargo { get; set; }
-        public string Email { get; set; }
-        public string Telefonos { get; set; }
-        public string Estado { get; set; }
+        public string? EntidadId { get; set; }
+        public string? Nombres { get; set; }
+        public string? Cargo { get; set; }
+        public string? Email { get; set; }
+        public string? Telefonos { get; set; }
+        public bool? Activo { get; set; }
+        public string? Estado { get; set; }
     }
 }
